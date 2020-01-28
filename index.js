e130036cbe11c0dfcda86251dd6f5a73a58d2c2d
Ville Paananen <villepaananen@outlook.com>
2020-01-28 15:35:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+  
+const express = require('express');
+const Datastore = require('nedb');
+
+const port = 3000;
+
+const app = express();
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+// Create the database
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+// Define the POST operation
+app.post('/api', (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+});
+
