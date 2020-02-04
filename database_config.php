f76c11e23b7df422e0967d412a6e6d83b0f6d32b
Ville Paananen <villepaananen@outlook.com>
2020-02-04 10:54:58 +0200

    database save added

---
+++ database_config.php
@@ -0,0 +1,8 @@
+<?php
+  $servername = "***REMOVED***";
+  $port = ***REMOVED***;
+  $username = "***REMOVED***";
+  $password = "***REMOVED***";
+  $dbname = "***REMOVED***";
+  $table = "tablename";
+?>
