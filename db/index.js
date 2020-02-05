05cece09336e546599e87dc3aaf40b76f1733f7b
Ville Paananen <villepaananen@outlook.com>
2020-02-05 15:06:07 +0200

    db fixes

--- db/index.js
+++ db/index.js
@@ -1,39 +1,22 @@
-const { Pool } = require("pg");
+const { Client } = require("pg");
 
-const pool = new Pool({
+const client = new Client({
   user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_SERVER_NAME,
   port: process.env.PORT,
   database: process.env.DATABASE_NAME,
   connectionString: process.env.DATABASE_URL,
-  max: 20,
   ssl: true
 });
 
-pool.on("error", (err, client) => {
-  console.error("Unexpected error on idle client", err);
-  process.exit(-1);
-});
-
-(async () => {
-  const client = await pool.connect();
-  try {
-    const res = await client.query(query);
-  } catch (e) {
-    throw e;
-  } finally {
-    client.release();
-  }
-})().catch(e => console.error(e.stack));
-
 module.exports = {
-  query: (text, params, callback) => {
-    const start = Date.now();
-    return pool.query(text, params, (err, res) => {
-      const duration = Date.now() - start;
-      console.log("executed query", { text, duration, rows: res.rowCount });
-      callback(err, res);
-    });
+  query: (text, params) => {
+    client.connect();
+    client
+      .query(text, params)
+      .then(result => console.log(results))
+      .catch(e => console.error(e.stack))
+      .then(() => client.end());
   }
 };
