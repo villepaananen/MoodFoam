59e8fc6a96c754f678fc6dfe14a167e5e8101098
Ville Paananen <villepaananen@outlook.com>
2020-02-13 14:42:37 +0200

    ssl fix

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { Client } = require("pg");
 
 const client = new Client({
   connectionString: process.env.DATABASE_URI,
-  ssl: false
+  ssl: true
 });
 
 async function insert(text, params) {
