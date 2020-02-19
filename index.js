5c54181030c0feb82d281db3ebfc77ed090dab54
Ville Paananen <villepaananen@outlook.com>
2020-02-19 16:04:05 +0200

    cookies added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const db = require("./database.js");
 const express = require("express");
+var cookieParser = require("cookie-parser");
 
 const port = process.env.PORT || 5500;
 
+const cookieoptions = {
+  maxAge: 1000 * 60 * 60 * 24 * 14
+};
+
 const app = express();
+app.use(cookieParser());
 app.listen(port, () => console.log(`Moodfoam app listening on port ${port}!`));
 app.use(express.static("public"));
 app.use(
@@ -15,15 +21,32 @@ app.use(
 
 // add the experiment response to the database
 app.post("/", (req, res) => {
-  db.none("INSERT INTO ***REMOVED***(response, timestamp) VALUES($1, $2)", [
+  let cookieid = req.cookies.id;
+  if (cookieid === undefined) {
+    cookieid = getRandomNumber();
+    res.cookie("id", cookieid, cookieoptions);
+    res.send("cookie set");
+    console.log("Cookie created successfully", cookieid);
+  } else {
+    console.log("Cookie exists", cookieid);
+  }
+  db.none("INSERT INTO ***REMOVED***(response, timestamp, id) VALUES($1, $2, $3)", [
     { data: req.body },
-    new Date()
+    new Date(),
+    cookieid
   ])
     .then(() => {
       console.log("DB insert success");
+      //res.send("Response sent successfully");
     })
     .catch(err => {
       console.log(err);
     });
-  res.redirect(307, "end.html");
+  //res.redirect(307, "end.html");
 });
+
+function getRandomNumber() {
+  let randomNumber = Math.random().toString();
+  randomNumber = randomNumber.substring(2, randomNumber.length);
+  return randomNumber;
+}
